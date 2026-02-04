bc8311a4f8260a495d3f2a807667db5ca0e21c6b
testsecret2026-crypto <testsecret2026@gmail.com>
2026-02-04 14:48:02 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js - 直接放在你的 GitHub 專案中
+const firebaseConfig = {
+  apiKey: "AIzaSyA7F0JbTqI5THGUQnqp7_BSBALAQQeIAk",
+  authDomain: "testsystem-2056d.firebaseapp.com",
+  projectId: "testsystem-2056d",
+  storageBucket: "testsystem-2056d.firebasestorage.app",
+  messagingSenderId: "1085110426660",
+  appId: "1:1085110426660:web:48af6f7864bc567f536ccb1"
+};
